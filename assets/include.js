70a64df88da9b433b0ce795b8866c2744a92c25c
Xavier Silva <xavierwsilva+git@gmail.com>
2026-01-27 17:27:54 -0500

    Load header with js

---
+++ assets/include.js
@@ -0,0 +1,9 @@
+async function loadComponent(id, file) {
+  const response = await fetch(file);
+  const html = await response.text();
+  document.getElementById(id).innerHTML = html;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("header", "/assets/header.html");
+});
